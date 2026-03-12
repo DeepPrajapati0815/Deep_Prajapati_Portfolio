9e3908b4dcd5dc625590b8eafb933cecf536a7f6
DeepPrajapati0815 <deepprajapati0815@gmail.com>
2026-03-13 00:59:16 +0530

    env changed

--- src/components/sections/contact.tsx
+++ src/components/sections/contact.tsx
@@ -67,7 +67,7 @@ export function ContactSection() {
 
     const serviceId = import.meta.env.VITE_EMAILJS_SERVICE_ID;
     const templateId = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
-    const publicKey = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
+    const publicKey = import.meta.env.VITE_EMAILJS_EMAIL_KEY;
 
     if (!serviceId || !templateId || !publicKey) {
       toast({
